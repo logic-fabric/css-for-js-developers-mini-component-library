45067b49d2464341fc1cdb07dfd4535d71b6c20e
logic-fabric <lowic@logic-fabric.fr>
2024-02-13 18:02:27 +0100

    refactor (Header): adapt Storybook initial code

--- stories/Header.stories.js
+++ stories/Header.stories.js
@@ -1,12 +1,10 @@
-import { Header } from './Header';
+import { Header } from '../components/molecules/Header/Header';
 
 export default {
   title: 'Example/Header',
   component: Header,
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
     layout: 'fullscreen',
   },
 };
--- stories/Page.jsx
+++ stories/Page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { Header } from './Header';
+import { Header } from '../components/molecules/Header/Header';
+
 import './page.css';
 
 export const Page = () => {
